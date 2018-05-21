fac83a7917c26fe963c07853e14d7206df6e118d
stargod <173193161@qq.com>
2018-05-22 00:30:19 +0800

    user

--- src/main/java/com/example/HelloworldApplication.java
+++ src/main/java/com/example/HelloworldApplication.java
@@ -1,9 +1,11 @@
-package com.example.helloworld;
+package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.dao")//将项目中对应的mapper类的路径加进来就可以了
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/dao/UserMapper.java
@@ -0,0 +1,21 @@
+package com.example.dao;
+
+import com.example.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+    //这个方式我自己加的
+    List<User> selectAllUser();
+}
